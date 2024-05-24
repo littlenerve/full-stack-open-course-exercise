f1b46fd25dc2fc4747fdc683d6bee22a7efe0128
littlenerve <1664176122@qq.com>
2024-05-24 14:55:04 +0800

    1.6

--- part1/Course-Information/src/App.jsx
+++ part1/Course-Information/src/App.jsx
@@ -1,54 +1,42 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Head text={"give feed back"}></Head>
+      <Button handleClick={() => setGood(good + 1)} text={"good"}></Button>
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"}></Button>
+      <Button handleClick={() => setBad(bad + 1)} text={"bad"}></Button>
+      <Head text={"statics"}></Head>
+      <Show desc={"good"} socre={good}></Show>
+      <Show desc={"neutral"} socre={neutral}></Show>
+      <Show desc={"bad"} socre={bad}></Show>
     </div>
   )
 }
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
+const Head = ({ text }) => (
+  <div>
+    <h1>{text}</h1>
+  </div>
+)
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises :{props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-    </div>
-  )
-}
+const Button = ({handleClick,text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
-      <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
-      <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
-    </div>
-  )
-}
+const Show = ({ desc, socre }) => (
+  <div>
+    <p>{desc} {socre}</p>
+  </div>
+)
+
+
 export default App
